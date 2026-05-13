694ac21a6225af3ba59599d21e4e2775bb978526
Isaque <isaquecross15@gmail.com>
2026-05-12 21:20:01 -0300

    Adiciona configuração de swagger para ser possível adicionar o token para fazer request direto do Swagger

---
+++ src/main/java/com/raizesdonordeste/infrastructure/security/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package com.raizesdonordeste.infrastructure.security;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+    info = @Info(
+        title = "Raízes do Nordeste API",
+        version = "1.0",
+        description = "API Back-end da rede de lanchonetes Raízes do Nordeste"
+    )
+)
+@SecurityScheme(
+    name = "bearerAuth",
+    type = SecuritySchemeType.HTTP,
+    scheme = "bearer",
+    bearerFormat = "JWT"
+)
+public class SwaggerConfig {
+}
